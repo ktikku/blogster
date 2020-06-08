929b6a5e6491909837294c651e501dfa07efec05
Karan Tikku <50620678+c285803@users.noreply.github.com>
2020-06-08 15:09:30 +0530

    initial commit

---
+++ src/components/showBlogs.vue
@@ -0,0 +1,80 @@
+<template>
+<div v-theme:column="'wide'" id="show-blogs">
+    <h1>All Blogs Articles</h1>
+    <input type="text" v-model="search" placeholder="Search Blogs" />
+    <div v-for="blog in filteredBlogs" class="single-blog">
+        <h2 v-rainbow>{{ blog.title | to-uppercase}}</h2>
+        <article>{{ blog.body | snippet }}</article>
+    </div>
+</div>
+</div>
+</template>
+
+<script>
+import searchMixin from '../mixins/searchMixin.js';
+
+export default {
+    data() {
+        return {
+            blogs: [],
+            search: ''
+        }
+    },
+    methods: {
+
+    },
+    created() {
+        this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function (data) {
+            this.blogs = data.body.slice(0, 10);
+        })
+    },
+    computed: {
+
+    },
+    filters: {
+        'to-uppercase': function (value) {
+            return value.toUpperCase();
+        },
+        'snippet': function (value) {
+            return value.slice(0, 100) + '...';
+        }
+    },
+    directives: {
+        'theme': {
+            bind(el, binding, vnode) {
+                if (binding.value === 'wide') {
+                    el.style.maxWidth = '1200px'
+                } else if (binding.value === 'narrow') {
+                    el.style.maxWidth = '560px'
+                }
+
+                if (binding.arg === 'column') {
+                    el.style.background = '#ddd';
+                    el.style.padding = '20px';
+
+                }
+            }
+        },
+        'rainbow': {
+            bind(el, binding, vnode) {
+                el.style.color = "#" + Math.random().toString().slice(2, 8);
+            }
+        }
+    },
+    mixins: [searchMixin]
+}
+</script>
+
+<style>
+#show-blogs {
+    max-width: 800px;
+    margin: 0 auto;
+}
+
+.single-blog {
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background: #eee
+}
+</style>
